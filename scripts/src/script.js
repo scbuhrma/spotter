f68d82cab6199acae98eef9a078888b754469230
Steven Buhrman <scbuhrma@gmail.com>
2012-02-16 22:52:58 -0500

    Addition on search field and facebook connect

--- scripts/src/script.js
+++ scripts/src/script.js
@@ -2,23 +2,23 @@
 function main() {
     
     
-     var s  = new Spotter("twitter.search",
-	{q:"Asheville",period:120},
+     var s  = new Spotter("facebook.search",
+	{q:"Disc Golf",period:120},
 	{buffer:true,bufferTimeOut:750});
     
 	var count = true;
 	var twitCount = 1;
-    s.register(function(tweet) {
+    s.register(function(status) {
     	
-        var profile_image = "<img src=\"" + tweet.profile_image_url+ "\" />";
+        var profile_image = "<img src=\"" + status.profile_image_url+ "\" />";
 	if(twitCount==11){
 	    $("#tweets p:last-child").remove();
             twitCount=10;
 	}
 	if(count){
-	$("#tweets").prepend($("<p class='green'>"+profile_image+tweet.text+"</p>").slideDown());
+	$("#tweets").prepend($("<p class='green'>"+profile_image+status.text+"</p>").slideDown());
 	}else{
-	$("#tweets").prepend($("<p class='teal'>"+profile_image+tweet.text+"</p>").slideDown());
+	$("#tweets").prepend($("<p class='teal'>"+profile_image+status.text+"</p>").slideDown());
 	}
         count = !count;	 	
     	twitCount++;
